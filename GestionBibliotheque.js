b9902cb879097d7823a8850c45d7b856ceaea458
vnci12 <mohayowane@gmail.com>
2025-12-04 16:12:32 +0300

    nouveau fichier créer et exercice fait

---
+++ GestionBibliotheque.js
@@ -0,0 +1,12 @@
+let totalLivres = 500;
+totalLivres+= 50
+totalLivres-= 10
+totalLivres+= 5
+
+console.log(totalLivres);
+
+let affichageTotalLivres = "La bibliothèque contient ";
+affichageTotalLivres+= totalLivres;
+affichageTotalLivres+= " livres au total."
+
+console.log(affichageTotalLivres);
