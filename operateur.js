191ef7b0846ecd3bbc584fc3ac7bcfe2b4508855
vnci12 <mohayowane@gmail.com>
2025-12-08 16:36:33 +0300

    nouvel methode

---
+++ operateur.js
@@ -0,0 +1,17 @@
+console.log("Ici je code les opérateur en html.");
+
+//addition//
+
+let prixUnitaire = 3;
+let valeurAjouter = 7;
+
+//premiére technique: additionner des chiffre//
+let prixAdditionne1 = 3+7;
+
+//j'affiche la valeur de la variable prixAdditionnel//
+console.log("prixAdditionne1: ", prixAdditionne1);
+
+//Deuxiéme methode d'additionner des nombres a l'aide variables//
+let prixAdditionne2 = prixUnitaire + valeurAjouter;
+
+console.log("prixAdditionne2: ", prixAdditionne2);
