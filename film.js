4a822bbcc65a624c3b40ea603f98b621bfca3ef4
vnci12 <mohayowane@gmail.com>
2025-12-10 16:34:12 +0300

    travail sur un tp en javascript

---
+++ film.js
@@ -0,0 +1,14 @@
+let ticket = {
+    nomFilm: "Koungou",
+    prix: "7€",
+    numeroSalle: "salle numéro 2",
+
+};
+
+let nom = "Yanis"
+
+let texteAffiche = "Bonjour " +  nom + 
+", votre film" + " " + ticket.nomFilm +" " + ticket.numeroSalle;
+
+console.log(ticket)
+console.log(texteAffiche)
