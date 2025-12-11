eadd07646b006d05f154cd8d1b6c5f2fa83ff87d
vnci12 <mohayowane@gmail.com>
2025-12-11 15:14:19 +0300

    tp sur le calcul d'age

--- boucle.js
+++ boucle.js
@@ -21,3 +21,10 @@ for(let indice = 0; indice < 3; indice++) {
 
 //Application de la boucle For sur une liste d'élèves
 let tableauEleves = ["Bounou-Ben", "Mohamed", "shahida", "Mounawara", "Anrabia", "Anzilati", "Nael", "Yowane", "Nouran", "Lidya", "Rachid", "Jassem"]
+
+console.log("taiile du tableau:", tableauEleves.length)
+console.log(tableauEleves[0]);
+console.log(tableauEleves[1]);
+console.log(tableauEleves[2]);
+console.log(tableauEleves[3]);
+
