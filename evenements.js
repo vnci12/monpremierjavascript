71197c9202a137ab7cb07835303b71502fb405ee
vnci12 <mohayowane@gmail.com>
2025-12-17 16:51:38 +0300

    ajout de span dans html et de nouveau élèment dans javascript

--- evenements.js
+++ evenements.js
@@ -18,6 +18,40 @@ const maDate = new Date();
 elementValiderDate.addEventListener("click", function() {
     //je decris laction au clic du bouton, wqj'insere la date du jour dans le span
     elementSpanDateDuJour.textContent = dateActuelle.toLocaleString();
-    
+
     elementSpanDateduJourFormat2.textContent = maDate.toLocaleDateString();
 });
+
+//evenement "keydown"
+
+//1. recupere lelement qui sera associé à l'evenement keydown
+let elementBouttonFleche = document.querySelector("#bouttonFleche");
+
+//2. j'associe l'evenement keydown à l'element recupere avc addEventListener(). a linterieur de la méthode addEventListener(), je passe une fonction fléchée comme suit '( => {})'.
+elementBouttonFleche.addEventListener("keydown", (event) =>{
+    console.log("evenement keydown");
+    console.log("touche appuyée :", event.key);
+});
+
+/**
+ * mon evenement "change"
+ * je code un evenement "change" sur un input de type text
+ * l'évenement "change" se déclenche lorsque l'utilisateur modifie la valeur d'un champ de formulaire et quitte ce champ (par exemple, en cliquant ailleurs ou en appuyant sur la touche Tab).
+*/
+
+//1. je récupere l'element html ou sera associé l'evenement "change"
+let elementInputNomForm = document.querySelector("#nom");
+
+//2. j'associe l'evenement "change" à l'element recupere avec addEventListener(), puis je console.log la nouvelle valeur de l'input a chaque changement de valeur.
+let messageSalutation; // je déclare une variable vide.
+
+let elementSpanNomUtilisateur = document.querySelector("#nomUtilisateur");
+//j'affiche le message de salutation dans le span correspondant
+
+elementSpanNomUtilisateur.textContent = messageSalutation;
+elementInputNomForm.addEventListener("change", (event) => {
+    console.log("nouvelle valeur :", event.target.value);
+    messageSalutation = `Bonjours ${event.target.value}`; // j'affecte une valeur à la variable vide.
+    elementSpanNomUtilisateur.textContent = messageSalutation;
+});
+
